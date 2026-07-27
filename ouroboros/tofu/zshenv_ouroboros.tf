f01a537312b5628644c6156cdcbb7059b8be4b5b
Paul Dejean <paulcdejean@gmail.com>
2026-07-26 23:03:49 -0500

    AI told me to add more to .gitignore

--- ouroboros/tofu/zshenv_ouroboros.tf
+++ ouroboros/tofu/zshenv_ouroboros.tf
@@ -1,4 +1,4 @@
-# The very .zshenv file that need to authenticate to infisical!
+# The very .zshenv file being used to authenticate to infisical!
 # The machine identity isn't a secret. You can't get in without an established AWS authentication.
 resource "local_sensitive_file" "zshenv" {
   content = templatefile("${path.module}/templates/zshenv.tftpl", {
